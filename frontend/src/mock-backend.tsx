80fbd7d0818f3a0b3502ec95b0c05cec81b1437a
jacob-sahl <jacobsahlmueller@gmail.com>
2021-10-24 18:09:52 -0400

    Split up types and switched to camelCase

--- frontend/src/mock-backend.tsx
+++ frontend/src/mock-backend.tsx
@@ -1,44 +1,11 @@
+/* eslint-disable sort-imports */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 
-// Interfaces go here:
-
-export interface Node {
-  id: number;
-  name: string;
-  image: string;
-  subnodes: Array<number>;
-  // access_level: number;
-  editors: Array<number>;
-  type: string;
-}
-
-export interface Subnode {
-  id: number;
-  // access_level: number;
-  editors: Array<number>;
-  type: string;
-  content: unknown;
-}
-
-export interface Game {
-  nodes: Array<number>;
-  players: Array<number>;
-  gms: Array<number>;
-  users: Array<number>; // = players + gms, might be a better way to define this
-  settings: Record<string, unknown>;
-}
-
-export interface User {
-  id: number;
-  username: string;
-  password: string; // FOR NOW
-  email: string; // FOR NOW
-  games: Array<number>;
-}
+import { Node, Subnode, Game, User } from './types';
 
 // Hardcoded variables go here:
-const global_subnodes = [
+const globalSubnodes = [
   {
     id: 1,
     node_id: 1,
@@ -62,7 +29,7 @@ const global_subnodes = [
   },
 ];
 
-const global_nodes = [
+const globalNodes = [
   {
     id: 1,
     name: 'The Soaring Skies',
@@ -74,7 +41,7 @@ const global_nodes = [
   },
 ];
 
-const global_users = [
+const globalUsers = [
   {
     id: 1,
     username: 'user1',
@@ -91,7 +58,7 @@ const global_users = [
   },
 ];
 
-const game_1 = {
+const game1 = {
   nodes: [1],
   players: [1],
   gms: [2],
@@ -101,14 +68,14 @@ const game_1 = {
 
 // Functions mocking backend behaviour go here:
 
-export const GET_node_by_id = (id: number): Node => {
-  return global_nodes.filter((node) => node.id == id)[0];
+export const GETnodeById = (id: number): Node => {
+  return globalNodes.filter((node) => node.id == id)[0];
 };
 
 export const GET_user_by_id = (id: number): User => {
-  return global_users.filter((user) => user.id == id)[0];
+  return globalUsers.filter((user) => user.id == id)[0];
 };
 
-export const GET_subnodes_by_node_id = (node_id: number): Array<Subnode> => {
-  return global_subnodes.filter((subnode) => subnode.node_id == node_id);
+export const GET_subnodes_by_node_id = (node_id: number): Subnode[] => {
+  return globalSubnodes.filter((subnode) => subnode.node_id == node_id);
 };
