9ab249b3d425950b4b5150638271af936025d43c
Filip Balucha <balucha.filip2@gmail.com>
2021-10-24 23:09:42 -0400

    Implement basic footer structure

--- frontend/src/components/CanvasSidebar/CanvasSidebar.tsx
+++ frontend/src/components/CanvasSidebar/CanvasSidebar.tsx
@@ -1,6 +1,6 @@
 import './canvasSidebar.css';
 import { Avatar, Button, IconButton, TextField } from '@mui/material';
-import { Delete, HighlightOff, Person, PersonAdd, PersonOutline } from '@mui/icons-material';
+import { Delete, HighlightOff, PersonAdd, PersonOutline } from '@mui/icons-material';
 import { Component } from 'react';
 import { User } from '../../types';
 // eslint-disable-next-line
@@ -26,12 +26,12 @@ export default class CanvasSidebar extends Component<Props> {
         <div className="game-name">The best game</div>
         <div className="player-list">
           {this.tempPlayers.map((player: User) => (
-            <div key={uuid()} className="player">
+            <div key={uuid()} className="player-card">
               <IconButton aria-label="Remove player" component="span">
                 <PersonOutline />
               </IconButton>
               <Avatar>{player.username.charAt(0).toUpperCase()}</Avatar>
-              {`@${player.username}`}
+              <div className="player-card__name">{`@${player.username}`}</div>
               <div className="button--remove">
                 <IconButton aria-label="Remove player" component="span">
                   <HighlightOff />
@@ -41,13 +41,24 @@ export default class CanvasSidebar extends Component<Props> {
           ))}
         </div>
         <div className="footer">
-          <TextField id="outlined-basic" label="Player name" variant="outlined" />
-          <Button startIcon={<PersonAdd />} variant="contained" aria-label="invite user to the game">
-            Invite user
-          </Button>
-          <Button startIcon={<Delete />} variant="contained" aria-label="delete game server">
-            Delete server
-          </Button>
+          <div className="footer__item__wrapper">
+            <TextField className="footer__item" id="outlined-basic" label="Player name" variant="outlined" />
+          </div>
+          <div className="footer__item__wrapper">
+            <Button
+              className="footer__item"
+              startIcon={<PersonAdd />}
+              variant="contained"
+              aria-label="invite user to the game"
+            >
+              Invite user
+            </Button>
+          </div>
+          <div className="footer__item__wrapper">
+            <Button className="footer__item" startIcon={<Delete />} variant="contained" aria-label="delete game server">
+              Delete server
+            </Button>
+          </div>
         </div>
       </div>
     );
