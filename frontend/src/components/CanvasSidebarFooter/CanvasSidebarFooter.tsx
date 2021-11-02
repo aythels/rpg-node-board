d4e7df86b63df5dbcdcfdf97fbc0e5072841a8c7
Filip Balucha <balucha.filip2@gmail.com>
2021-11-02 19:48:07 -0400

    Align the styling of invite player text field with edit game text field

--- frontend/src/components/CanvasSidebarFooter/CanvasSidebarFooter.tsx
+++ frontend/src/components/CanvasSidebarFooter/CanvasSidebarFooter.tsx
@@ -1,5 +1,5 @@
 import './canvasSidebarFooter.css';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, IconButton, Tooltip } from '@mui/material';
 import { ChangeEvent, Component } from 'react';
 import { Delete, PersonAdd } from '@mui/icons-material';
 import Dialog from '../Dialog/Dialog';
@@ -26,50 +26,43 @@ export default class CanvasSidebarFooter extends Component<Props, State> {
   render(): JSX.Element {
     return (
       <div className="canvas-sidebar-footer">
-        <div className="item__wrapper">
-          <div className="text-field__wrapper">
-            <TextField
-              autoComplete="off"
-              className="text-field"
-              id="outlined-basic"
-              label="Enter player name"
-              value={this.state.inviteName}
-              variant="outlined"
-              onChange={this.handleInviteNameChanged}
-              onKeyPress={(event) => {
-                if (event.key === 'Enter') {
-                  event.preventDefault();
-                  this.props.onInvitePlayerClicked(this.state.inviteName);
-                }
-              }}
-            />
-          </div>
-        </div>
-        <div className="item__wrapper">
-          <Button
-            aria-label="invite user to the game"
-            className="item"
-            disabled={!this.state.inviteName}
-            startIcon={<PersonAdd />}
-            variant="contained"
-            onClick={() => {
-              this.props.onInvitePlayerClicked(this.state.inviteName);
+        <div className="text-field__wrapper">
+          <TextField
+            autoComplete="off"
+            className="text-field"
+            id="outlined-basic"
+            label="Enter player name"
+            value={this.state.inviteName}
+            variant="outlined"
+            onChange={this.handleInviteNameChanged}
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                event.preventDefault();
+                this.props.onInvitePlayerClicked(this.state.inviteName);
+              }
             }}
-          >
-            Invite player
-          </Button>
+          />
+          <Tooltip arrow title="Invite player">
+            <IconButton
+              aria-label="Invite player to the game"
+              component="span"
+              disabled={!this.state.inviteName}
+              onClick={() => this.props.onInvitePlayerClicked(this.state.inviteName)}
+            >
+              <PersonAdd />
+            </IconButton>
+          </Tooltip>
         </div>
-        <div className="item__wrapper">
-          <Button
-            aria-label="delete game server"
-            className="item delete-button"
-            startIcon={<Delete />}
-            variant="contained"
-            onClick={() => this.setState({ showDeleteServerDialog: true })}
-          >
-            Delete server
-          </Button>
-        </div>
+        <Button
+          aria-label="delete game server"
+          className="delete-button"
+          startIcon={<Delete />}
+          variant="contained"
+          onClick={() => this.setState({ showDeleteServerDialog: true })}
+        >
+          Delete server
+        </Button>
+
         <Dialog
           description="Doing so will immediately end the session and remove the game from the server."
           header="Delete server?"
