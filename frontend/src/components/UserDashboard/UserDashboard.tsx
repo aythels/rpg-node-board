c345ede86864e8f5098239abbb6a8fb6974412e4
Camran Hansen <camran.hansen@mail.utoronto.ca>
2021-11-07 16:22:58 -0500

    Version 1 of dashboard

--- frontend/src/components/UserDashboard/UserDashboard.tsx
+++ frontend/src/components/UserDashboard/UserDashboard.tsx
@@ -1,7 +1,9 @@
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { GameRow } from '../GameRow/gamerow';
 import { Component } from 'react';
+import './UserDashboard.css';
 import { GETuserById } from '../../mock-backend';
 
 interface Props {
@@ -10,9 +12,19 @@ interface Props {
 export default class UserDashboard extends Component<Props> {
   render(): JSX.Element {
     return (
-      <div>
-        <h1>Welcome, {GETuserById(this.props.userID).username} </h1>
-        <GameRow userID={this.props.userID} />
+      <div className="padded_div">
+        <Grid container direction="row" justifyContent="space-between" alignItems="flex-start">
+          <Grid item>
+            <h1>Welcome, {GETuserById(this.props.userID).username} </h1>
+          </Grid>
+          <Grid item>
+            <SettingsIcon fontSize="large" />
+          </Grid>
+          <Grid item container>
+            <h2>My games </h2>
+            <GameRow userID={this.props.userID} />
+          </Grid>
+        </Grid>
       </div>
     );
   }
